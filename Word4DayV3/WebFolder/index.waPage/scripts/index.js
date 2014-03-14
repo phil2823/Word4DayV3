f2efa4ad775b0f967337126706f9f0e07abb4253
phil2823 <philip2823@gmail.com>
2014-03-14 18:06:11 -0400

    WFD Update2

---
+++ Word4DayV3/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,32 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var menuItem2 = {};	// @menuItem
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	menuItem2.click = function menuItem2_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		//$$('menuItem2').setState('active');	// ends up this does not work
+		//need to put in a function to reset all the menus back to normal
+		//$$('menuItem1').setTextColor('black');
+		//$$('menuItem1').setBackgroundColor('white');
+		$$('menuItem2').setTextColor('orange');
+		$$('menuItem2').setBackgroundColor("blue");
+		//$$('menuItem3').setTextColor('black');
+		//$$('menuItem3').setBackgroundColor('white');
+		//$$('menuItem4').setTextColor('black');
+		//$$('menuItem4').setBackgroundColor('white');
+		
+		
+		//alert('ouch');   
+		$$('mainComponent').loadComponent('/views/wordgroups.waComponent'); //should load up wordgroups
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("menuItem2", "click", menuItem2.click, "WAF");
+// @endregion
+};// @endlock
