49a944219595ffe65d15ce62ac8d332fc5819dd1
phil2823 <philip2823@gmail.com>
2014-03-19 13:06:51 -0400

    WFDVersion3
    This has the basic gui with students, early version of word groups and more (goign to change word groups in the next version to an accordian)

--- Word4DayV3/WebFolder/index.waPage/scripts/index.js
+++ Word4DayV3/WebFolder/index.waPage/scripts/index.js
@@ -2,11 +2,62 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuItem5 = {};	// @menuItem
+	var menuItem4 = {};	// @menuItem
+	var menuItem3 = {};	// @menuItem
 	var menuItem1 = {};	// @menuItem
 	var menuItem2 = {};	// @menuItem
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	menuItem5.click = function menuItem5_click (event)// @startlock
+	{// @endlock
+		// system menu item (Aztec only) pop an error if not aztec
+		$$('menuItem1').setTextColor('black');
+		$$('menuItem1').setBackgroundColor('white');
+		$$('menuItem2').setTextColor('black');
+		$$('menuItem2').setBackgroundColor("white");
+		$$('menuItem3').setTextColor('black');
+		$$('menuItem3').setBackgroundColor('white');
+		$$('menuItem4').setTextColor('black');
+		$$('menuItem4').setBackgroundColor('white');
+		$$('menuItem5').setTextColor('blue');
+		alert('Warning: a check will go in here for Aztec Login only');   
+		$$('mainComponent').loadComponent('/views/system.waComponent'); //should load up system
+	};// @lock
+
+	menuItem4.click = function menuItem4_click (event)// @startlock
+	{// @endlock
+		// reports Menu Item
+		$$('menuItem1').setTextColor('black');
+		$$('menuItem1').setBackgroundColor('white');
+		$$('menuItem2').setTextColor('black');
+		$$('menuItem2').setBackgroundColor("white");
+		$$('menuItem3').setTextColor('black');
+		$$('menuItem3').setBackgroundColor('white');
+		$$('menuItem4').setTextColor('orange');
+		$$('menuItem4').setBackgroundColor('white');
+		$$('menuItem5').setTextColor('black');
+		//alert('ouch');   
+		$$('mainComponent').loadComponent('/views/reports.waComponent'); //should load up reports
+	};// @lock
+
+	menuItem3.click = function menuItem3_click (event)// @startlock
+	{// @endlock
+		// Curricula Menu Item
+		$$('menuItem1').setTextColor('black');
+		$$('menuItem1').setBackgroundColor('white');
+		$$('menuItem2').setTextColor('black');
+		$$('menuItem2').setBackgroundColor("white");
+		$$('menuItem3').setTextColor('orange');
+		$$('menuItem3').setBackgroundColor('white');
+		$$('menuItem4').setTextColor('black');
+		$$('menuItem4').setBackgroundColor('white');
+		$$('menuItem5').setTextColor('black');
+		//alert('ouch');   
+		$$('mainComponent').loadComponent('/views/curr.waComponent'); //should load up curricula
+	};// @lock
 
 	menuItem1.click = function menuItem1_click (event)// @startlock
 	{// @endlock
@@ -19,8 +70,9 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		$$('menuItem3').setBackgroundColor('white');
 		$$('menuItem4').setTextColor('black');
 		$$('menuItem4').setBackgroundColor('white');
+		$$('menuItem5').setTextColor('black');
 		//alert('ouch');   
-		$$('mainComponent').loadComponent('/views/students.waComponent'); //should load up wordgroups
+		$$('mainComponent').loadComponent('/views/students.waComponent'); //should load up students
 	};// @lock
 
 	menuItem2.click = function menuItem2_click (event)// @startlock
@@ -36,11 +88,15 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		$$('menuItem3').setBackgroundColor('white');
 		$$('menuItem4').setTextColor('black');
 		$$('menuItem4').setBackgroundColor('white');
+		$$('menuItem5').setTextColor('black');
 		//alert('ouch');   
 		$$('mainComponent').loadComponent('/views/wordgroups.waComponent'); //should load up wordgroups
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("menuItem5", "click", menuItem5.click, "WAF");
+	WAF.addListener("menuItem4", "click", menuItem4.click, "WAF");
+	WAF.addListener("menuItem3", "click", menuItem3.click, "WAF");
 	WAF.addListener("menuItem1", "click", menuItem1.click, "WAF");
 	WAF.addListener("menuItem2", "click", menuItem2.click, "WAF");
 // @endregion
